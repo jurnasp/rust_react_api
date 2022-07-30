cf45b3f7cc96e6f36cc1c90e4c0eeb96c24149c3
Jürnas Pluutus <JyrnasPluutus@gmail.com>
2022-07-30 21:57:42 +0300

    Add postgres and add first route running test query

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use] extern crate rocket;
+
 use serde::Serialize;
 use rocket::serde::json::Json;
+use rocket_db_pools::{sqlx, Database, Connection};
+use rocket_db_pools::sqlx::Row;
 
 #[derive(Serialize)]
 struct User{
@@ -15,14 +18,33 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/api", routes![user])
-}
-
 #[get("/user")]
 fn user() -> Json<User> {
     Json(User{name: "John".to_string(), age: 22, address: "Paris".to_string()})
+}
+
+#[get("/db")]
+async fn db(mut db: Connection<PgDatabase>) -> Result<Json<i32>, &'static str> {
+    let result:i32 = sqlx::query("SELECT 1+1 AS result")
+        .fetch_one(&mut *db)
+        .await
+        .and_then(|r| Ok(r.get("result")))
+        .unwrap();
+
+    Ok(Json(result))
+}
+
+#[derive(Database)]
+#[database("pg_db")]
+struct PgDatabase(sqlx::PgPool);
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    rocket::build()
+        .attach(PgDatabase::init())
+        .mount("/", routes![index])
+        .mount("/api", routes![user, db])
+        .launch()
+        .await;
 }
